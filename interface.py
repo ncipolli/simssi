59125ab1076bc8c7425cd769720a659a9bdc6e34
N Cipolli <49487964+ncipolli@users.noreply.github.com>
2021-09-10 23:41:35 -0300

    melhorias, conexão com codigo necessária

--- interface.py
+++ interface.py
@@ -1,71 +1,72 @@
-from tkinter import *
-
+import tkinter as tk
+from tkinter import messagebox
 import codigo
+
 
 class Application:
     def __init__(self, master=None):
 
         # dados gerais
 
-        self.SIMSSI = Label(text="SIMSSI")
+        self.SIMSSI = tk.Label(text="SIMSSI")
         self.SIMSSI["font"] = ("Verdana", "10", "bold")
 
         # titulo sem. otimizado
-        self.SemOtm = Label(text="Dados com Semáforo Otimizado")
+        self.SemOtm = tk.Label(text="Dados com Semáforo Otimizado")
         self.SemOtm["font"] = ("Verdana", "10", "bold")
 
         # dados das vias com uso dos semáforos com otimização
 
-        self.SOtxt1 = Label(text="Avenida 1")
-        self.SOvol1 = Label(text="" + str(codigo.val1[0]))
-        self.SOcond1 = Label(text="" + str(codigo.val1[1]))
-        self.SOsin1 = Label(text="" + str(codigo.val1[2]))
-        self.SObt1 = Button(text="Parada", command=self.mudarTexto)
+        self.SOtxt1 = tk.Label(text="Avenida 1")
+        self.SOvol1 = tk.Label(text="1")
+        self.SOcond1 = tk.Label(text="2")
+        self.SOsin1 = tk.Label(text="3")
+        self.SObt1 = tk.Button(text="Parada", command=self.botaoA)
 
-        self.SOtxt2 = Label(text="Avenida 2")
-        self.SOvol2 = Label(text="" + str(codigo.val2[0]))
-        self.SOcond2 = Label(text="" + str(codigo.val2[1]))
-        self.SOsin2 = Label(text="" + str(codigo.val2[2]))
-        self.SObt2 = Button(text="Parada", command=self.mudarTexto)
+        self.SOtxt2 = tk.Label(text="Avenida 2")
+        self.SOvol2 = tk.Label(text="1")
+        self.SOcond2 = tk.Label(text="2")
+        self.SOsin2 = tk.Label(text="3")
+        self.SObt2 = tk.Button(text="Parada", command=self.botaoB)
 
-        self.SOtxt3 = Label(text="Avenida 3")
-        self.SOvol3 = Label(text="" + str(codigo.val3[0]))
-        self.SOcond3 = Label(text="" + str(codigo.val3[1]))
-        self.SOsin3 = Label(text="" + str(codigo.val3[2]))
-        self.SObt3 = Button(text="Parada", command=self.mudarTexto)
+        self.SOtxt3 = tk.Label(text="Avenida 3")
+        self.SOvol3 = tk.Label(text="1")
+        self.SOcond3 = tk.Label(text="2")
+        self.SOsin3 = tk.Label(text="3")
+        self.SObt3 = tk.Button(text="Parada", command=self.botaoA)
 
-        self.SOtxt4 = Label(text="Avenida 4")
-        self.SOvol4 = Label(text="" + str(codigo.val4[0]))
-        self.SOcond4 = Label(text="" + str(codigo.val4[1]))
-        self.SOsin4 = Label(text="" + str(codigo.val4[2]))
-        self.SObt4 = Button(text="Parada", command=self.mudarTexto)
+        self.SOtxt4 = tk.Label(text="Avenida 4")
+        self.SOvol4 = tk.Label(text="1")
+        self.SOcond4 = tk.Label(text="2")
+        self.SOsin4 = tk.Label(text="3")
+        self.SObt4 = tk.Button(text="Parada", command=self.botaoB)
 
 
         # row 3 - titulo sem. comum
-        self.SemCom = Label(text="Dados com Semáforo Comum")
+        self.SemCom = tk.Label(text="Dados com Semáforo Comum")
         self.SemCom["font"] = ("Verdana", "10", "bold")
 
         # row 4 - dados das vias com uso dos semáforos comuns
-        self.SCtxt1 = Label(text="Avenida 1")
-        self.SCvol1 = Label(text="volume 1")
-        self.SCsin1 = Label(text="sinal 1")
+        self.SCtxt1 = tk.Label(text="Avenida 1")
+        self.SCvol1 = tk.Label(text="volume 1")
+        self.SCsin1 = tk.Label(text="sinal 1")
 
-        self.SCtxt2 = Label(text="Avenida 2")
-        self.SCvol2 = Label(text="volume 2")
-        self.SCsin2 = Label(text="sinal2")
+        self.SCtxt2 = tk.Label(text="Avenida 2")
+        self.SCvol2 = tk.Label(text="volume 2")
+        self.SCsin2 = tk.Label(text="sinal2")
 
-        self.SCtxt3 = Label(text="Avenida 3")
-        self.SCvol3 = Label(text="volume 3")
-        self.SCsin3 = Label(text="sinal 3")
+        self.SCtxt3 = tk.Label(text="Avenida 3")
+        self.SCvol3 = tk.Label(text="volume 3")
+        self.SCsin3 = tk.Label(text="sinal 3")
 
-        self.SCtxt4 = Label(text="Avenida 4")
-        self.SCvol4 = Label(text="volume 4")
-        self.SCsin4 = Label(text="sinal 4")
+        self.SCtxt4 = tk.Label(text="Avenida 4")
+        self.SCvol4 = tk.Label(text="volume 4")
+        self.SCsin4 = tk.Label(text="sinal 4")
 
         # row 5 - botões de sair e simulação
-        self.sair = Button(text="Sair", width=5)
+        self.sair = tk.Button(text="Sair", width=5, command=self.ExitApp)
 
-        self.simulacao = Button(text="Simulação")
+        self.simulacao = tk.Button(text="Simulação")
 
         #criação das grids
         self.SIMSSI.grid(row = 0, column = 0, columnspan= 3, padx = 10, pady = 10)
@@ -120,19 +121,30 @@ class Application:
         self.simulacao.grid(row=11, column=2)
 
 
+    def botaoA(self):
+        MsgBox = tk.messagebox.askquestion('SIMSSI','Tem certeza que quer acionar a parada dessa via?',icon = 'error')
+        if MsgBox == 'yes':
+            self.botaoA = "parar"
+        return self.botaoA
 
-    def mudarTexto(self):
-        if self.SOtxt1["text"] == "Atualizado":
-            self.SOtxt1["text"] = "Aguardando atualização"
-        else:
-            self.SOtxt1["text"] = "Atualizado"
+    def botaoB(self):
+        MsgBox = tk.messagebox.askquestion('SIMSSI','Tem certeza que quer acionar a parada dessa via?',icon = 'error')
+        if MsgBox == 'yes':
+            self.botaoB = "parar"
+        return self.botaoB
+
+    def ExitApp(self):
+        MsgBox2 = tk.messagebox.askquestion('SIMSSI','Sair do aplicativo?',icon = 'error')
+        if MsgBox2 == 'yes':
+            root.destroy()
 
 
 
 
-root = Tk()
+root = tk.Tk()
 root.title("SIMSSI - Controle do Usuário")
 root.geometry("400x400")
 
 Application(root)
+
 root.mainloop()
